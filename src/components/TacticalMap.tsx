bb6ddef0eb995b19f4f598ca4653a6669b76b2aa
Aga <ahmadgadamu@gmail.com>
2026-04-23 00:49:31 +0100

    feat: Switch to standard OpenStreetMap tiles
    Reverts custom dark map styling in favor of default OpenStreetMap tiles. This change simplifies map rendering and removes potentially conflicting custom CSS filters.

--- src/components/TacticalMap.tsx
+++ src/components/TacticalMap.tsx
@@ -122,8 +122,8 @@ export default function TacticalMap({
         className="w-full h-full"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <div className="absolute top-4 left-1/2 -translate-x-1/2 z-[1000] flex gap-4 pointer-events-none">
